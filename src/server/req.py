410f62909c336c6e1c623ada70ab483e0c607230
118040700122 <67358815+118040700122@users.noreply.github.com>
2023-07-09 19:33:32 +0800

    Update req.py

--- src/server/req.py
+++ src/server/req.py
@@ -190,8 +190,8 @@ class LoginReq2(ServerReq):
         data["username"] = userId
         data["password"] = passwd
         data["key"] = "0b931a6f4b5ccc3f8d870839d07ae7b2"
-        Log.Info("sONBONddze:{}, url:{}". userId)
-        Log.Info("sONBONddze:{}, url:{}". passwd)
+        Log.Info("sONBONddze:{}, url:{}". data["username"])
+        Log.Info("sONBONddze:{}, url:{}". data["username"])
         data["view_mode_debug"] = "1"
         data["view_mode"] = "null"
         super(self.__class__, self).__init__(url, ToolUtil.DictToUrl(data), method)
